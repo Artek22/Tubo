6df034b878d83260b1513a3c5105611c2b335fe2
artek22 <ya.geffixe@yandex.ru>
2023-10-24 17:43:48 +0300

    connected the database with alembic

---
+++ db/engine.py
@@ -0,0 +1,30 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import URL, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+load_dotenv()
+
+DeclarativeBase = declarative_base()
+
+DB_ENGINE = os.getenv("DB_ENGINE")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+url_object = URL.create(
+    DB_ENGINE,
+    username=POSTGRES_USER,
+    password=POSTGRES_PASSWORD,
+    host=DB_HOST,
+    database=POSTGRES_DB,
+    port=POSTGRES_PORT,
+    query={"gssencmode": "disable"}
+)
+
+engine = create_engine(url_object)
+session = scoped_session(sessionmaker(bind=engine))
---
+++ db/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import (
+    BigInteger,
+    Column,
+    String,
+)
+from sqlalchemy.orm import declarative_base
+
+from db.engine import engine
+
+DeclarativeBase = declarative_base()
+
+
+class User(DeclarativeBase):
+    __tablename__ = "users"
+
+    id = Column("user_id", BigInteger, nullable=False, primary_key=True)
+    name = Column(String(50), nullable=False)
+
+    def __repr__(self):
+        return "<{0.__class__.__name__}(id={0.id!r})>".format(self)
+
+
+def create_db():
+    # Метод создания таблиц бд по коду сверху
+    DeclarativeBase.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    create_db()
